176f8064afff49fcd78df4c1f80b8567e233a823
Andrew Mark (CR/AEA2) <Mark.Andrew@de.bosch.com>
2017-02-27 18:10:21 +0100

    playing with interfaces and type checking

--- routes/bla.ts
+++ routes/bla.ts
@@ -4,9 +4,22 @@ import {C1} from '../classes/C1'
 
 export var bla = Router();
 
+interface I1 {
+  prop: number;
+}
+
+function doit(x : {prop: number}) {
+    x.prop += 110;
+}
+
+function more(x : I1) {
+  doit(x);
+}
 
 /* GET home page. */
 bla.get('/', function(req, res, next) {
+  let myObj = {a: 1, x: 2, prop: 2}
+  more(myObj)
   let c1 = new C1(1,"a", false);
   let c2 = C1.make({c: "bla"});
   let n = C1.count;
