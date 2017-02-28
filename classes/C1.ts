aec3e19b7ddc47774a5132988188643a051c3bcc
Andrew Mark (CR/AEA2) <Mark.Andrew@de.bosch.com>
2017-02-28 19:06:43 +0100

    all sorts of new snippets

--- classes/C1.ts
+++ classes/C1.ts
@@ -17,9 +17,58 @@ export class C1 {
         return new C1(obj.a, obj.c, true);
     }
 
+    add(x,y) { return x + y} // crappy old untyped js
 
+    derived() {
+        var a = {a:1}
+        var b = {}
+        //a = {}           // these guys are illegal
+        //b.foo = 1
+    }
+    
 }
 
+enum E {A,B,C}
+
+enum F {D,E,F}
 
 let c = new C1(99, "hi", false);
+
+let e = E.A
+
+let b = e
+
+let s: string = E[E.A]
+
+function f (x: E): number {
+    console.log(x.toString())
+    return x + 1
+} 
+
+f(b);
+
+function overl (n :number) : number;
+function overl (b : boolean): boolean;
+function overl (x: number | boolean ){
+    if (typeof x === "boolean") {
+        return !x
+    } else {
+        return x + 1;
+    }
+}
+
+let ores = overl(1)
+
+ores = 1;
+
+function callsig (tst: string, fun: (text: string) => number): number {
+    return fun(tst);
+}
+
+function ln(s: string): number {
+    return s.length
+}
+
+var nres = callsig("foobar", ln);
+
 
