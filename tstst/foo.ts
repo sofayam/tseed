3bf5908958dd169f9e1dc971f1bef218dcbd6e44
Mark Andrew <mark.andrew@de.bosch.com>
2019-03-19 11:46:23 +0100

    stable

--- tstst/foo.ts
+++ tstst/foo.ts
@@ -3,7 +3,7 @@ import * as express from "express";
 
 class Foo {
     name: string;
-    arr: [string]
+    arr: string[]
     a1: number[]
 
 }
@@ -23,3 +23,59 @@ f.a1[0] = 3
 let e = express()
 
 console.log("hi")
+
+function inc(a: number) {
+    return a + 1;
+}
+var x = inc(1)
+
+//x += "fs"
+
+interface I {
+    arr: number[],
+    str: string,
+}
+
+var xi: I = {
+    arr: [1,2,3],
+    str: "foo",
+}
+
+function foo(x: I): string {
+    // x.arr[0] = "bar"
+    // return x.arr[2];
+    return x.str
+}
+
+interface Cat {
+    type: "cat",
+    mice_caught: number;
+}
+interface Dog {
+    type: "dog",
+    bones_gnawed: number;
+}
+
+type Pet = Cat | Dog;
+
+function details(p: Pet): string {
+    if (p.type == "dog") {
+        return p.bones_gnawed + " bones "
+    }
+    else {
+        return p.mice_caught + " mice "
+    }
+}
+
+interface Animal {
+    name: string;
+}
+
+type Creature = Pet & Animal;
+
+var moggie: Creature = {
+    name: "felix",
+    type: "cat",
+    mice_caught: 10,
+}
+
